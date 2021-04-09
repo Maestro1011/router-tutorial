e7a21d9c7cfffda39b618c31914b3457c9fb1c96
Maestro1011 <m2makamaestro@gmail.com>
2021-04-09 22:20:46 +0530

    Added project files

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Nav from './Nav';
+import About from './About';
+import Shop from './Shop';
+import Home from './Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'; 
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+    <div className="App">
+      <Nav />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/shop" component={Shop}/>
+      </Switch>
+    </div>
+    </Router>
+  );
+}
+
+export default App;
